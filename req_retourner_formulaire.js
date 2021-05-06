8eecffb886fedf7842d8f613380a4ee39d39fcb5
BENAISSA <BENAISSA@intech-s1>
2021-05-06 12:42:08 +0200

    ajout des "req" et modification des" modele_"

---
+++ req_retourner_formulaire.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const fs = require("fs");
+require('remedial');
+
+const trait = function (req, res, query) {
+
+    let marqueurs;
+    let page;
+
+
+    page = fs.readFileSync('modele_formulaire_inscription.html', 'utf-8');
+
+    marqueurs = {}; 
+    marqueurs.erreur = ""; 
+    marqueurs.pseudo = ""; 
+    page = page.supplant(marqueurs);
+
+    res.writeHead(200, { 'Content-Type': 'text/html' }); 
+    res.write(page);
+    res.end();
+};
+//--------------------------------------------------------------------------
+
+module.exports = trait;
+~                                        
