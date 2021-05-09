13ae34f4591fc8226e84adc2205735fb4e1998aa
DEVRED <DEVRED@intech-s1>
2021-05-09 19:40:31 +0200

    ajout de req multiple (non fonctionnel)

--- req_afficher_choix_niveau.js
+++ req_afficher_choix_niveau.js
@@ -5,15 +5,10 @@ require('remedial');
 
 const trait = function (req, res, query) {
 
-    let marqueurs;
     let page;
 
     page = fs.readFileSync('modele_choix_niveau.html', 'utf-8');
 
-    marqueurs = {}; 
-    marqueurs.erreur = ""; 
-    marqueurs.pseudo = ""; 
-    page = page.supplant(marqueurs);
 
     res.writeHead(200, { 'Content-Type': 'text/html' }); 
     res.write(page);
---
+++ req_multiple.js
@@ -0,0 +1,31 @@
+'use strict'
+require('remedial')
+const fs = require("fs");
+
+const req_multiple = function (req, res, query) {
+	let contenu;
+	let page;
+	let marqueurs;
+	let indice;
+	let questions;
+
+	contenu = fs.readFileSync("questions_multiples.json", "utf-8");
+	questions = JSON.parse(contenu);
+	
+	indice = Math.floor(Math.random() * questions.length);
+	
+	marqueurs = {};
+	marqueurs.intitule = questions[indice].question;
+	marqueurs.reponse0 = questions[indice].prop0;
+	marqueurs.reponse1 = questions[indice].prop1;
+	marqueurs.reponse2 = questions[indice].prop2;
+	marqueurs.reponse3 = questions[indice].prop3;
+	page = fs.readFileSync("modele_multiple.html", "utf-8");
+	page = page.supplant(marqueurs);
+
+	res.writeHead(200, { "content-type": "text/html" });
+	res.write(page);
+	res.end();
+};
+
+module.exports = req_multiple;
