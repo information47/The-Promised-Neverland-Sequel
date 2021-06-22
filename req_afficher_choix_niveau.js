b4169449f4cfa14fb2646ee3b24f7c48f7996a34
TRAN <TRAN@intech-s1>
2021-06-22 15:15:59 +0200

    ajout de champs

--- req_afficher_choix_niveau.js
+++ req_afficher_choix_niveau.js
@@ -6,8 +6,12 @@ require('remedial');
 const trait = function (req, res, query) {
 
     let page;
+	let marqueurs;
 
+	marqueurs = {};
+	marqueurs.pseudo = query.pseudo;
     page = fs.readFileSync('modele_choix_niveau.html', 'utf-8');
+	page = page.supplant(marqueurs);
 
 
     res.writeHead(200, { 'Content-Type': 'text/html' }); 
